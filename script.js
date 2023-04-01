5fe3826ddb77344acd91df5fce146e835cb36fa7
Falconic-coder <mohammad.ahmad852@yahoo.com>
2023-04-01 22:19:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function getComputerChoice(){
+    let choices = ["rock", "paper", "scissor"];
+    return choices[Math.floor(Math.random()*3)];
+}
+
+
+let HSCore = 0;
+let CScore = 0;
+let rounds = 0;
+let prevChoice = ["", ""];
+
+
+
+function changeColour(colour, parentClass, choice){
+    let doc = document.querySelector(parentClass+"> .IconRPS > ."+choice);
+    doc.querySelector("button").style.backgroundColor = colour;
+    doc.querySelector("button>img").style.backgroundColor = colour;
+}
+
+
+function playGame(choice){
+
+    if(prevChoice[0].length != 0)
+    {
+        changeColour("white", ".human", prevChoice[0]);
+        changeColour("white", ".computer", prevChoice[1]);
+    }
+
+    let humanChoice = choice;
+    prevChoice[0] = humanChoice;
+    changeColour("rgb(179, 179, 255)", ".human", prevChoice[0]);
+
+    let CompChoice = getComputerChoice();
+    prevChoice[1] = CompChoice;
+    changeColour("rgb(243, 84, 84)", ".computer", prevChoice[1]);
+
+    CompChoice = CompChoice.charAt(0);
+    humanChoice = humanChoice.charAt(0);
+
+    if(humanChoice != CompChoice)
+    {
+        if((humanChoice == 'r' && CompChoice == 's') || (humanChoice == 'p' && CompChoice == 'r') || (humanChoice == 's' && CompChoice == 'p'))
+        {
+            HSCore++;
+            document.getElementById("HCount").innerHTML = "Count : "+HSCore;
+        } 
+
+        else
+        {
+            CScore++;
+            document.getElementById("CCount").innerHTML = "Count : "+CScore;
+        }
+    }
+
+    rounds++;
+}
